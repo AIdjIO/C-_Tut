8f777cb77b1c9c65c653cee275572f9c2ff37877
AIdjIO <ai.dj@pm.me>
2020-07-06 13:00:42 +0100

    added OOP, updated README

--- C++_OOP.cpp
+++ C++_OOP.cpp
@@ -1,1 +1,28 @@
 //C++ OOP
+
+#include <iostream>
+using namespace std;
+
+//C++ Classes and Objects
+
+//Create a Class
+class myClass {	//the class
+	public:		//access specifier
+	int myNum;
+	string myString;
+};
+
+//Create an object (instance of the class)
+int main(){
+	 myClass myObj; //create an object of myClass
+	 
+	 //access atributes and set values
+	 myObj.myNum = 15;
+	 myObj.myString ="Some text";
+	 
+	 //Print attribute values
+	 cout << myObj.myNum << "\n";
+	 cout << myObj.myString << "\n";
+	 return 0;
+}
+
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ---
-C++ Self Learning (Self documenting)
+C++ Self Learning
 ---
+Placeholder repository for my own documentation
 ![C++](https://user-images.githubusercontent.com/67799618/86537589-05f73a80-bee8-11ea-9be3-9dfe4eaf5be5.jpg)
 ---
 What is C++?
